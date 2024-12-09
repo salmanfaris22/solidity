c14003d998be430355abc87a878f9dfa84d1dc8c
salmanfaris22 <salman567633800@gmail.com>
2024-12-09 16:41:35 +0530

    "cal"

--- contracts/Calculetor.sol
+++ contracts/Calculetor.sol
@@ -1,11 +1,30 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.26;
 
-
 contract Calculeto {
- uint256 result = 0;
+    // State variable to store the result
+    uint256  result =0;
 
- function add()public {
-    result+=10;
- }
+ 
+    function add(uint256 num) public {
+        result += num;
+    }
+
+    function subtract(uint256 num) public {
+        result -= num;
+    }
+
+  
+    function multiply(uint256 num) public {
+        result *= num;
+    }
+
+  
+    function get() public view returns (uint256) {
+        return result;
+    }
+
+    function reset() public {
+        result = 0;
+    }
 }
